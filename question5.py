36a94ccf4ff76e573e5c7765ee786ecfcbed0d33
Batuhan Özbay <62551738+kakan18@users.noreply.github.com>
2022-04-16 16:59:42 +0300

    Update question5.py

--- question5.py
+++ question5.py
@@ -1,4 +1,4 @@
 import pandas as pd
-df = pd.read_csv('country_vaccination_stats.csv')
+df = pd.read_csv('country_vaccination_stats.csv') #imputed version of dataset
 clistdf = df.groupby(['country'])['daily_vaccinations'].median().reset_index()
 print(clistdf.nlargest(3,'daily_vaccinations'))
